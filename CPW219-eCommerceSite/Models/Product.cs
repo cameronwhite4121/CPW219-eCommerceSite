5ae390f73f686cca84bfd66fab132e129b06d500
Cameron White <cameronwhite4121@gmail.com>
2024-07-31 17:01:20 -0700

    Added a class for products

---
+++ CPW219-eCommerceSite/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPW219_eCommerceSite.Models
+{
+    /// <summary>
+    /// Represents a product that is sold at Cam's Consoles
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// The primary key for each product
+        /// </summary>
+        [Key]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Name of the product
+        /// </summary>
+        [Required]
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Sale cost of the product
+        /// </summary>
+        [Required]
+        [Range(0, 10000)]
+        public double Price { get; set; }
+    }
+}
