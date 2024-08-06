e0f806fec58d787ecb41737166d2388c51ae7149
Cameron White <cameronwhite4121@gmail.com>
2024-08-06 13:36:23 -0700

    Made view for create product

---
+++ CPW219-eCommerceSite/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219_eCommerceSite.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Create() 
+        {
+            return View();
+        }
+    }
+}
