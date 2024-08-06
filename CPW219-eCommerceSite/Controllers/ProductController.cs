bbe65a461b95e3b04e5b2149b8e89e1850b38320
Cameron White <cameronwhite4121@gmail.com>
2024-08-06 14:07:55 -0700

    Create view is now fully functional

--- CPW219-eCommerceSite/Controllers/ProductController.cs
+++ CPW219-eCommerceSite/Controllers/ProductController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CPW219_eCommerceSite.Data;
+using CPW219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CPW219_eCommerceSite.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly ProductContext _context;
+        public ProductController(ProductContext context) 
+        {
+            _context = context;
+        }
         public IActionResult Create() 
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if(ModelState.IsValid)
+            {
+                // Add to db
+                _context.products.Add(product);
+                _context.SaveChanges();
+                // Show message on page
+                ViewData["Message"] = $"{product.Name} was added";
+                return View();
+            }
+            
+            return View(product);
         }
     }
 }
