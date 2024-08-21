08e9fcf1bec870a352fc9e01e57ab3622044e8ae
Cameron White <cameronwhite4121@gmail.com>
2024-08-20 23:16:30 -0700

    Register form is functional, username and phone are now nullable

--- CPW219-eCommerceSite/Controllers/MembersController.cs
+++ CPW219-eCommerceSite/Controllers/MembersController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CPW219_eCommerceSite.Data;
+using CPW219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CPW219_eCommerceSite.Controllers
 {
     public class MembersController : Controller
     {
+
+        private readonly ProductContext _context;
+        public MembersController(ProductContext context) 
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel m)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add member to the database
+                Member newMember = new()
+                {
+                    Email = m.Email,
+                    Password = m.Password,
+                };
+
+                _context.members.Add(newMember);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(m);
         }
     }
 }
--- CPW219-eCommerceSite/Models/Member.cs
+++ CPW219-eCommerceSite/Models/Member.cs
@@ -11,9 +11,9 @@ namespace CPW219_eCommerceSite.Models
 
         public string Password { get; set; }
 
-        public string UserName { get; set; }
+        public string? UserName { get; set; }
 
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
     }
 
     [Microsoft.EntityFrameworkCore.Keyless]
@@ -31,11 +31,13 @@ namespace CPW219_eCommerceSite.Models
 
         [Required]
         [StringLength(75, MinimumLength = 6)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required]
         [Compare(nameof (Password))]
         [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
     }
 }
